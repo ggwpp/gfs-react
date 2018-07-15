c4a64888d69b4e6358dc133a3a640f5009338bee
patipat <patipat@omise.co>
2018-07-15 16:02:01 +0700

    content from avgle

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import Card from './Card'
+import axios from 'axios'
 
-const cards = ['A','B','C','D','E','F']
+
+const cards = []
+const api_video_cat = 'https://api.avgle.com/v1/categories'
+const api_video_tsubasa = 'https://api.avgle.com/v1/search/Tsubasa%20Amami/0'
 
 class Product extends Component {
 
@@ -9,8 +13,26 @@ class Product extends Component {
     super(props)
     this.state = {
       cards: cards
-    }
+    }    
   }
+
+  componentDidMount() {
+    this.getVideoCat()
+  }
+
+  getVideoCat(){
+    axios.get(api_video_tsubasa)
+    // .then((res) => console.log(res.data))
+    .then((res) => 
+        res.data.response.videos.map(data => (        
+          console.log(data),
+          cards.push(data),
+          this.setState({ card: data })
+        )
+      )
+    )
+  }
+
 
   addClicked = e => {
     console.log("Add was clicked !!")
@@ -30,7 +52,13 @@ class Product extends Component {
         <div class="columns" style={{ display: 'flex', flexFlow: 'row wrap' }} >        
             {cards.map(card => (
                 <div class="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd" >                
-                <Card name={card}/>
+                <Card name={card.title}
+                      cover_img={card.preview_url}
+                      sub_name={card.viewnumber}
+                      ext_link={card.embedded_url}                                          
+                      fps = {card.framerate}
+                      like  = {card.likes}
+                />
                 </div>
             ))}
             
